7a039f856daa6b92d2a42f9488eeb0d13ddaf56a
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2022-02-17 16:10:34 +0300

    emrpove code

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 
 myList = ["Ahmed", "Mohammed", "Mona", "Khareem", "Moayed", "Khadeeja", "Salim"]
 
-filterList = list(filter(lambda name: (name[0]=="K" and name[1] == "h") or (name[0] =="M" and name[1] == "o") , myList))
+filterList = list(filter(lambda name: name.startswith("Kh") or name.startswith("Mo") , myList))
 print(filterList)
